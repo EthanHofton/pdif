884286d8596aa68ac13242bead41efdccc41da8a
Ethan Hofton <ethanhofton@gmail.com>
2023-12-27 19:34:24 +0000

    fixed configure files

--- cli/src/main.cpp
+++ cli/src/main.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+#include <pdif_cli/pdif_cli_config.hpp>
+#include <pdif/test.hpp>
+
 int main(int argc, char** argv)
 {
-    printf("Hello World!\n");
+    printf("pdif_cli version %s.%s.%s\n", PDIF_CLI_VERSION_MAJOR, PDIF_CLI_VERSION_MINOR, PDIF_CLI_VERSION_PATCH);
+    printf("%d", test());
     return 0;
 }
