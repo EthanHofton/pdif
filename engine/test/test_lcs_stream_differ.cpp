ff8ad5df623c61dbb00483bccfb31c850ef66e10
ethan hofton <ethanhofton@gmail.com>
2024-03-08 13:42:59 +0000

    added more tests to lcs_stream_differ

--- engine/test/test_lcs_stream_differ.cpp
+++ engine/test/test_lcs_stream_differ.cpp
@@ -191,6 +191,159 @@ TEST(PDIFLcsStreamDiffer, TestBigChange) {
     ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::DELETE);
 }
 
+TEST(PDIFLcsStreamDiffer, TestAddFont) {
+    pdif::stream stream1;
+    pdif::stream stream2;
+
+    stream1.push_back(pdif::stream_elem::create<pdif::font_elem>("Arial", 12));
+    stream1.push_back(pdif::stream_elem::create<pdif::text_elem>("Hello"));
+    stream1.push_back(pdif::stream_elem::create<pdif::text_elem>("Hello"));
+
+    stream2.push_back(pdif::stream_elem::create<pdif::font_elem>("Arial", 12));
+    stream2.push_back(pdif::stream_elem::create<pdif::text_elem>("Hello"));
+    stream2.push_back(pdif::stream_elem::create<pdif::font_elem>("CM10", 12));
+    stream2.push_back(pdif::stream_elem::create<pdif::text_elem>("Hello"));
+
+    pdif::lcs_stream_differ differ(stream1, stream2);
+    pdif::diff diff;
+
+    ASSERT_NO_THROW(differ.diff(diff));
+
+    ASSERT_EQ(diff.edit_op_size(), 4);
+
+    auto edit_op = diff.get_edit_op(0);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+    ASSERT_EQ(edit_op.has_arg(), false);
+
+    edit_op = diff.get_edit_op(1);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+    ASSERT_EQ(edit_op.has_arg(), false);
+
+    edit_op = diff.get_edit_op(2);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::INSERT);
+    ASSERT_EQ(edit_op.has_arg(), true);
+    ASSERT_EQ(edit_op.get_arg()->as<pdif::font_elem>()->font_name(), "CM10");
+    ASSERT_EQ(edit_op.get_arg()->as<pdif::font_elem>()->font_size(), 12);
+
+    edit_op = diff.get_edit_op(3);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+    ASSERT_EQ(edit_op.has_arg(), false);
+}
+
+TEST(PDIFLcsStreamDiffer, TestAddTextColor) {
+    pdif::stream stream1;
+    pdif::stream stream2;
+
+    stream1.push_back(pdif::stream_elem::create<pdif::text_color_elem>(0, 0, 0));
+    stream1.push_back(pdif::stream_elem::create<pdif::text_elem>("Hello"));
+    stream1.push_back(pdif::stream_elem::create<pdif::text_elem>("World"));
+
+    stream2.push_back(pdif::stream_elem::create<pdif::text_color_elem>(0, 0, 0));
+    stream2.push_back(pdif::stream_elem::create<pdif::text_elem>("Hello"));
+    stream2.push_back(pdif::stream_elem::create<pdif::text_color_elem>(0, 1, 0));
+    stream2.push_back(pdif::stream_elem::create<pdif::text_elem>("World"));
+
+    pdif::lcs_stream_differ differ(stream1, stream2);
+
+    pdif::diff diff;
+    
+    ASSERT_NO_THROW(differ.diff(diff));
+
+    ASSERT_EQ(diff.edit_op_size(), 4);
+
+    auto edit_op = diff.get_edit_op(0);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+
+    edit_op = diff.get_edit_op(1);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+
+    edit_op = diff.get_edit_op(2);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::INSERT);
+    ASSERT_EQ(edit_op.has_arg(), true);
+    ASSERT_EQ(edit_op.get_arg()->as<pdif::text_color_elem>()->red(), 0);
+    ASSERT_EQ(edit_op.get_arg()->as<pdif::text_color_elem>()->green(), 1);
+    ASSERT_EQ(edit_op.get_arg()->as<pdif::text_color_elem>()->blue(), 0);
+
+    edit_op = diff.get_edit_op(3);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+}
+
+TEST(PDIFLcsStreamDiffer, TestAddStrokeColor) {
+    pdif::stream stream1;
+    pdif::stream stream2;
+
+    stream1.push_back(pdif::stream_elem::create<pdif::stroke_color_elem>(0, 0, 0));
+    stream1.push_back(pdif::stream_elem::create<pdif::text_elem>("Hello"));
+    stream1.push_back(pdif::stream_elem::create<pdif::text_elem>("World"));
+
+    stream2.push_back(pdif::stream_elem::create<pdif::stroke_color_elem>(0, 0, 0));
+    stream2.push_back(pdif::stream_elem::create<pdif::text_elem>("Hello"));
+    stream2.push_back(pdif::stream_elem::create<pdif::stroke_color_elem>(0, 1, 0));
+    stream2.push_back(pdif::stream_elem::create<pdif::text_elem>("World"));
+
+    pdif::lcs_stream_differ differ(stream1, stream2);
+
+    pdif::diff diff;
+    
+    ASSERT_NO_THROW(differ.diff(diff));
+
+    ASSERT_EQ(diff.edit_op_size(), 4);
+
+    auto edit_op = diff.get_edit_op(0);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+
+    edit_op = diff.get_edit_op(1);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+
+    edit_op = diff.get_edit_op(2);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::INSERT);
+    ASSERT_EQ(edit_op.has_arg(), true);
+    ASSERT_EQ(edit_op.get_arg()->as<pdif::stroke_color_elem>()->red(), 0);
+    ASSERT_EQ(edit_op.get_arg()->as<pdif::stroke_color_elem>()->green(), 1);
+    ASSERT_EQ(edit_op.get_arg()->as<pdif::stroke_color_elem>()->blue(), 0);
+
+    edit_op = diff.get_edit_op(3);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+}
+
+TEST(PDIFLcsStreamDiffer, TestAddXObjectImg) {
+    pdif::stream stream1;
+    pdif::stream stream2;
+
+    stream1.push_back(pdif::stream_elem::create<pdif::xobject_img_elem>("img1", 300, 300));
+    stream1.push_back(pdif::stream_elem::create<pdif::text_elem>("Hello"));
+    stream1.push_back(pdif::stream_elem::create<pdif::text_elem>("World"));
+
+    stream2.push_back(pdif::stream_elem::create<pdif::xobject_img_elem>("img1", 300, 300));
+    stream2.push_back(pdif::stream_elem::create<pdif::text_elem>("Hello"));
+    stream2.push_back(pdif::stream_elem::create<pdif::xobject_img_elem>("img2", 200, 200));
+    stream2.push_back(pdif::stream_elem::create<pdif::text_elem>("World"));
+
+    pdif::lcs_stream_differ differ(stream1, stream2);
+
+    pdif::diff diff;
+    
+    ASSERT_NO_THROW(differ.diff(diff));
+
+    ASSERT_EQ(diff.edit_op_size(), 4);
+
+    auto edit_op = diff.get_edit_op(0);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+
+    edit_op = diff.get_edit_op(1);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+
+    edit_op = diff.get_edit_op(2);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::INSERT);
+    ASSERT_EQ(edit_op.has_arg(), true);
+    ASSERT_EQ(edit_op.get_arg()->as<pdif::xobject_img_elem>()->image_hash(), "img2");
+    ASSERT_EQ(edit_op.get_arg()->as<pdif::xobject_img_elem>()->width(), 200);
+    ASSERT_EQ(edit_op.get_arg()->as<pdif::xobject_img_elem>()->height(), 200);
+
+    edit_op = diff.get_edit_op(3);
+    ASSERT_EQ(edit_op.get_type(), pdif::edit_op_type::EQ);
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
