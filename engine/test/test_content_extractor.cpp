16201e765cfa4c40637f17abe687dc96365d4412
ethan hofton <ethanhofton@gmail.com>
2024-03-16 10:04:06 +0000

    added almost all contest_extractor tests

--- engine/test/test_content_extractor.cpp
+++ engine/test/test_content_extractor.cpp
@@ -258,16 +258,236 @@ TEST(PDIFContentExtractor, Fonts) {
     ASSERT_EQ(s[10]->as<pdif::text_elem>()->text(), "1");
 }
 
-TEST(PDIFContentExtractor, Color) {}
-TEST(PDIFContentExtractor, Backslash) {}
+TEST(PDIFContentExtractor, Color) {
 
-TEST(PDIFContentExtractor, Pageno2) {}
-TEST(PDIFContentExtractor, Pageno3) {}
-TEST(PDIFContentExtractor, PagenoAll) {}
-TEST(PDIFContentExtractor, PagenoInvalid) {}
 
-TEST(PDIFContentExtractor, MultiPageScopeDocument) {}
-TEST(PDIFContentExtractor, MultiPageScopePage) {}
+    std::shared_ptr<QPDF> pdf = QPDF::create();
+    pdf->processFile("test_pdfs/font_change_red.pdf");
+
+    std::vector<pdif::stream> streams = pdif::extract_content(pdf, pdif::granularity::sentence, pdif::scope::page);
+
+    auto check_color = [&](int index, int r, int g, int b) {
+        ASSERT_EQ(streams[0][index]->type(), pdif::stream_type::text_color_set);
+        ASSERT_EQ(streams[0][index]->as<pdif::text_color_elem>()->red(), r);
+        ASSERT_EQ(streams[0][index]->as<pdif::text_color_elem>()->green(), g);
+        ASSERT_EQ(streams[0][index]->as<pdif::text_color_elem>()->blue(), b);
+    };
+
+    auto check_stroke = [&](int index, int r, int g, int b) {
+        ASSERT_EQ(streams[0][index]->type(), pdif::stream_type::stroke_color_set);
+        ASSERT_EQ(streams[0][index]->as<pdif::stroke_color_elem>()->red(), r);
+        ASSERT_EQ(streams[0][index]->as<pdif::stroke_color_elem>()->green(), g);
+        ASSERT_EQ(streams[0][index]->as<pdif::stroke_color_elem>()->blue(), b);
+    };
+
+    ASSERT_EQ(streams.size(), 1);
+
+    pdif::stream s = streams[0];
+    ASSERT_EQ(s.size(), 23);
+
+    check_color(0, 0, 0, 0);
+    check_stroke(1, 0, 0, 0);
+    check_color(2, 0, 0, 0);
+    check_stroke(3, 0, 0, 0);
+    check_color(4, 0, 0, 0);
+    check_stroke(5, 0, 0, 0);
+
+    ASSERT_EQ(s[6]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[6]->as<pdif::font_elem>()->font_name(), "CMR10");
+}
+
+TEST(PDIFContentExtractor, Backslash) {
+    std::shared_ptr<QPDF> pdf = QPDF::create();
+    pdf->processFile("test_pdfs/backslash.pdf");
+
+    std::vector<pdif::stream> streams = pdif::extract_content(pdf, pdif::granularity::sentence, pdif::scope::page);
+
+    ASSERT_EQ(streams.size(), 1);
+
+    pdif::stream s = streams[0];
+
+    ASSERT_EQ(s.size(), 7);
+
+    ASSERT_EQ(s[0]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_name(), "CMR10");
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_size(), 9);
+
+    ASSERT_EQ(s[1]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[1]->as<pdif::text_elem>()->text(), "This is an example of a blackslash:");
+
+    ASSERT_EQ(s[2]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[2]->as<pdif::font_elem>()->font_name(), "CMSY10");
+    ASSERT_EQ(s[2]->as<pdif::font_elem>()->font_size(), 9);
+
+    ASSERT_EQ(s[3]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[3]->as<pdif::text_elem>()->text(), "backslash");
+
+    ASSERT_EQ(s[4]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[4]->as<pdif::font_elem>()->font_name(), "CMR10");
+    ASSERT_EQ(s[4]->as<pdif::font_elem>()->font_size(), 9);
+
+    ASSERT_EQ(s[5]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[5]->as<pdif::text_elem>()->text(), ".");
+
+    ASSERT_EQ(s[6]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[6]->as<pdif::text_elem>()->text(), "1");
+}
+
+TEST(PDIFContentExtractor, Pageno2) {
+    std::shared_ptr<QPDF> pdf = QPDF::create();
+    pdf->processFile("test_pdfs/multi_page.pdf");
+
+    std::vector<pdif::stream> streams = pdif::extract_content(pdf, pdif::granularity::sentence, pdif::scope::page, 1);
+
+    ASSERT_EQ(streams.size(), 1);
+
+    pdif::stream s = streams[0];
+
+    ASSERT_EQ(s.size(), 3);
+
+    ASSERT_EQ(s[0]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_name(), "CMR10");
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_size(), 9);
+
+    ASSERT_EQ(s[1]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[1]->as<pdif::text_elem>()->text(), "This is the second page.");
+
+    ASSERT_EQ(s[2]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[2]->as<pdif::text_elem>()->text(), "2");
+}
+
+TEST(PDIFContentExtractor, Pageno3) {
+    std::shared_ptr<QPDF> pdf = QPDF::create();
+    pdf->processFile("test_pdfs/multi_page.pdf");
+
+    std::vector<pdif::stream> streams = pdif::extract_content(pdf, pdif::granularity::sentence, pdif::scope::page, 2);
+
+    ASSERT_EQ(streams.size(), 1);
+
+    pdif::stream s = streams[0];
+
+    ASSERT_EQ(s.size(), 3);
+
+    ASSERT_EQ(s[0]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_name(), "CMR10");
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_size(), 9);
+
+    ASSERT_EQ(s[1]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[1]->as<pdif::text_elem>()->text(), "This is the third page.");
+
+    ASSERT_EQ(s[2]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[2]->as<pdif::text_elem>()->text(), "3");
+}
+
+TEST(PDIFContentExtractor, PagenoInvalid) {
+    std::shared_ptr<QPDF> pdf = QPDF::create();
+    pdf->processFile("test_pdfs/multi_page.pdf");
+    
+    ASSERT_THROW({
+        pdif::extract_content(pdf, pdif::granularity::sentence, pdif::scope::page, 3);
+    }, std::runtime_error);
+}
+
+TEST(PDIFContentExtractor, PageNoAllMultiPageScopeDocument) {
+    std::shared_ptr<QPDF> pdf = QPDF::create();
+    pdf->processFile("test_pdfs/multi_page.pdf");
+
+    std::vector<pdif::stream> streams = pdif::extract_content(pdf, pdif::granularity::sentence, pdif::scope::document, -1);
+
+    ASSERT_EQ(streams.size(), 1);
+
+    pdif::stream s = streams[0];
+
+    ASSERT_EQ(s.size(), 9);
+
+    // Page 1
+
+    ASSERT_EQ(s[0]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_name(), "CMR10");
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_size(), 9);
+
+    ASSERT_EQ(s[1]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[1]->as<pdif::text_elem>()->text(), "This is the first page.");
+
+    ASSERT_EQ(s[2]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[2]->as<pdif::text_elem>()->text(), "1");
+
+    // Page 2
+
+    ASSERT_EQ(s[3]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[3]->as<pdif::font_elem>()->font_name(), "CMR10");
+    ASSERT_EQ(s[3]->as<pdif::font_elem>()->font_size(), 9);
+
+    ASSERT_EQ(s[4]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[4]->as<pdif::text_elem>()->text(), "This is the second page.");
+
+    ASSERT_EQ(s[5]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[5]->as<pdif::text_elem>()->text(), "2");
+
+    // Page 3
+
+    ASSERT_EQ(s[6]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[6]->as<pdif::font_elem>()->font_name(), "CMR10");
+    ASSERT_EQ(s[6]->as<pdif::font_elem>()->font_size(), 9);
+
+    ASSERT_EQ(s[7]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[7]->as<pdif::text_elem>()->text(), "This is the third page.");
+
+    ASSERT_EQ(s[8]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[8]->as<pdif::text_elem>()->text(), "3");
+}
+
+TEST(PDIFContentExtractor, PageNoAllMultiPageScopePage) {
+    std::shared_ptr<QPDF> pdf = QPDF::create();
+    pdf->processFile("test_pdfs/multi_page.pdf");
+
+    std::vector<pdif::stream> streams = pdif::extract_content(pdf, pdif::granularity::sentence, pdif::scope::page, -1);
+
+    ASSERT_EQ(streams.size(), 3);
+
+
+    // Page 1
+    pdif::stream s = streams[0];
+    ASSERT_EQ(s.size(), 3);
+
+    ASSERT_EQ(s[0]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_name(), "CMR10");
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_size(), 9);
+
+    ASSERT_EQ(s[1]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[1]->as<pdif::text_elem>()->text(), "This is the first page.");
+
+    ASSERT_EQ(s[2]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[2]->as<pdif::text_elem>()->text(), "1");
+
+    // Page 2
+    s = streams[1];
+    ASSERT_EQ(s.size(), 3);
+
+    ASSERT_EQ(s[0]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_name(), "CMR10");
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_size(), 9);
+
+    ASSERT_EQ(s[1]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[1]->as<pdif::text_elem>()->text(), "This is the second page.");
+
+    ASSERT_EQ(s[2]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[2]->as<pdif::text_elem>()->text(), "2");
+
+    // Page 3
+    s = streams[2];
+    ASSERT_EQ(s.size(), 3);
+
+    ASSERT_EQ(s[0]->type(), pdif::stream_type::font_set);
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_name(), "CMR10");
+    ASSERT_EQ(s[0]->as<pdif::font_elem>()->font_size(), 9);
+
+    ASSERT_EQ(s[1]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[1]->as<pdif::text_elem>()->text(), "This is the third page.");
+
+    ASSERT_EQ(s[2]->type(), pdif::stream_type::text);
+    ASSERT_EQ(s[2]->as<pdif::text_elem>()->text(), "3");
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
